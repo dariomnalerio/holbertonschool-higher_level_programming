b00f90627143b5b6b15771231263f06006054808
dariomnalerio <dariomnalerio@gmail.com>
2023-01-17 14:01:55 -0300

    Add task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        letter = ord(i)
+        if letter >= 97 and letter <= 122:
+            letter -= 32
+            print('{}'.format(chr(letter)), end="")
+    print()
