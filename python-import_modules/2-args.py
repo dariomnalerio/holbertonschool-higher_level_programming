8431d9156f010f5f9d0eb89e30329988c905dedc
dariomnalerio <dariomnalerio@gmail.com>
2023-01-22 16:00:57 -0300

    Task 2, one argument case fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if argNum == 0:
         print('0 arguments.')
     elif argNum == 1:
-        print('1 argument: {}' .format(argv[0]))
+        print('1 argument:')
     else:
         print('{} arguments:' .format(argNum))
     for i, arg in enumerate(argv, start=1):
