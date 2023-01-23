09dc8ec8f3777ed11a1e3e0773f1204674371aba
dariomnalerio <dariomnalerio@gmail.com>
2023-01-23 03:06:51 -0300

    Now works for last element of list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if my_list is None:
         return
     list_length = len(my_list)
-    if idx < 0 or idx > list_length:
+    if idx < 0 or idx >= list_length:
         return my_list
     new_list = my_list.copy()
     new_list[idx] = element
