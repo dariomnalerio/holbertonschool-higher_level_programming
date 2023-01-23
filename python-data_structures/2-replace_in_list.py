8600e0ee32ed92ae7fd255d044d11b9b5840b5f1
dariomnalerio <dariomnalerio@gmail.com>
2023-01-23 02:48:04 -0300

    Length variable fix

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    listLength = len(my_list) - 1
+    listLength = len(my_list)
     if idx > 0 or idx <= listLength:
         my_list[idx] = element
     return my_list
