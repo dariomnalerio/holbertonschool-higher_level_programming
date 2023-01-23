e4d887a9215e7c2978cc81b91dbd868bc6f77577
dariomnalerio <dariomnalerio@gmail.com>
2023-01-23 03:25:57 -0300

    Fix output format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    length = len(matrix)
     for row in matrix:
         for i in row:
-            print("{:d}".format(i), end=" ")
+            print("{:d}".format(i), end="")
+            if i != length:
+                print(end=" ")
         print()
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
