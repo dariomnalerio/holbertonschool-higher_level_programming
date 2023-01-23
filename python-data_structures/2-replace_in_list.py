b7b1247c7748a8f9ddba412dd8d85d10adfa4913
dariomnalerio <dariomnalerio@gmail.com>
2023-01-23 02:49:10 -0300

    Task logic change

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    listLength = len(my_list)
-    if idx > 0 or idx <= listLength:
+    listLength = len(my_list) - 1
+    if idx < 0 or idx > listLength:
+        return my_list
+    else:
         my_list[idx] = element
-    return my_list
+        return my_list
