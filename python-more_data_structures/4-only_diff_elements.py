c22d4823a7883753155d102e8b8ac96d9b89174e
dariomnalerio <dariomnalerio@gmail.com>
2023-01-23 21:47:58 -0300

    Add return comment

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,4 +2,4 @@
 def only_diff_elements(set_1, set_2):
     if set_1 is None or set_2 is None:
         return
-    return list(set(set_1) .symmetric_difference(set(set_2)))
+    return list(set(set_1) .symmetric_difference(set(set_2))) ## could return set_1 ^ set_2
