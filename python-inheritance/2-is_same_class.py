b15f00c8a14cfb9d3fbf0c2fc1254d900fa54e73
dariomnalerio <dariomnalerio@gmail.com>
2023-02-08 10:59:20 -0300

    Update task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+""" is_same_class function """
+
+
 def is_same_class(obj, a_class):
+    """Returns True if the object is exactly an instance of
+    the specified class; False otherwise"""
     return type(obj) is a_class
