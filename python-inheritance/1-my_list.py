195aec7bc31973ed33c049cad4260e565340f902
dariomnalerio <dariomnalerio@gmail.com>
2023-02-07 22:35:56 -0300

    Add task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Class MyList which inherits from list"""
+
+
+class MyList(list):
+    """ Class MyList which inherits from list"""
+
+    def print_sorted(self):
+        print(sorted(self))
