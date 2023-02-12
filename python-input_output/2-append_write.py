c8639415e795b438e735cee434131b15baca6be8
dariomnalerio <dariomnalerio@gmail.com>
2023-02-12 17:37:50 -0300

    Documentation update

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,6 +4,6 @@
 
 def append_write(filename="", text=""):
     """ appends a string at the end of a text file (UTF8)
-        and returns the number of characters added""""
+        and returns the number of characters added """
     with open(filename, mode='a', encoding='utf-8') as file:
         return file.write(text)
