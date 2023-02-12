6598863f7acfa949a05dd8426760d3c9ade005db
dariomnalerio <dariomnalerio@gmail.com>
2023-02-12 16:14:56 -0300

    Add task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module for reading a file """
+
+
+def read_file(filename=""):
+    """ Reads a text file (UTF8) and prints it to stdout """
+    with open(filename, encoding='uft-8') as file:
+        for lines in file:
+            print(lines, end="")
