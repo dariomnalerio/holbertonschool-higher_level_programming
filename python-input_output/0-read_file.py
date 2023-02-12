dfccebcef90cf550e9efc6fb4a1ed9f70aece80b
dariomnalerio <dariomnalerio@gmail.com>
2023-02-12 16:15:43 -0300

    Fix typo

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ Reads a text file (UTF8) and prints it to stdout """
-    with open(filename, encoding='uft-8') as file:
+    with open(filename, encoding='utf-8') as file:
         for lines in file:
             print(lines, end="")
