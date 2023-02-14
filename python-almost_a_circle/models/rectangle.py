c1b3b027302a88a7bb4fd07c25d600a1c0531abe
dariomnalerio <dariomnalerio@gmail.com>
2023-02-13 23:03:19 -0300

    Update Rectangle module

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """" Module containing rectangle class """
-
-
 from base import Base
 
 
@@ -9,8 +7,40 @@ class Rectangle(Base):
     """ Class defining a rectangle, inheriting from Base class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+    @property
+    def heigth(self):
+        return self._height
+
+    @heigth.setter
+    def height(self, value):
+        self._height = value
+
+    @property
+    def x(self):
+        return self.x
+
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+    @property
+    def y(self):
+        return self.y
+
+    @y.setter
+    def y(self, value):
+        self._y = value
