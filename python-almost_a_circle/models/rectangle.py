162bcde33803de200cdeb081b82f81d29d5a367e
dariomnalerio <dariomnalerio@gmail.com>
2023-02-14 12:31:00 -0300

    Update Rectangle __str__ method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,7 @@ class Rectangle(Base):
         else:
             for height in range(self.__height):
                 print("#" * self.__width)
+
+    def __str__(self):
+        """ Overrides the __str__ method """
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
