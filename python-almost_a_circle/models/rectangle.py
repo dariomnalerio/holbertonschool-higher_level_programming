95b95cfff6e7c4a6bf5107bac943d7fa87d7facd
dariomnalerio <dariomnalerio@gmail.com>
2023-02-15 16:07:04 -0300

    Add Rectangle update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,16 @@ class Rectangle(Base):
         width = self.__width
         height = self.__height
         return f"[Rectangle] ({id}) {x}/{y} - {width}/{height}"
+    
+    def update(self, *args):
+        """ Assigns an argument to each attribute """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
