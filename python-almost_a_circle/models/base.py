c5fc09e429a80ff9162b17c5bb3707fabaef8e1e
dariomnalerio <dariomnalerio@gmail.com>
2023-02-19 20:13:16 -0300

    Add task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Module containing class Base """
+import json
+from os import path
 
 
 class Base:
@@ -13,3 +15,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns the JSON string representation
+            of a list of dictionaries """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
