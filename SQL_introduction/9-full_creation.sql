be764c80064f1211389ab3b389a1e7235963ae6a
dariomnalerio <dariomnalerio@gmail.com>
2023-03-17 14:47:03 -0300

    Add documentation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,5 @@
+-- cript that creates a table second_table in the database
+-- hbtn_0c_0 in your MySQL server and add multiples rows
 CREATE TABLE IF NOT EXISTS second_table (
     id int,
     name VARCHAR(256),
