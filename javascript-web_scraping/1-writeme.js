094d889fc6dcbb6ea088dd43b8972f91383f6cdd
dariomnalerio <dariomnalerio@gmail.com>
2023-04-21 11:10:27 -0700

    Add task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Script that writes a string to a file
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
