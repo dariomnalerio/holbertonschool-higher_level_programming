0f4f48b8667a3e2395e97e0f4f0e046eb17b28b2
dariomnalerio <dariomnalerio@gmail.com>
2023-04-21 11:26:46 -0700

    Add task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Prints the title of a Star Wars movie where the episode number matches a given integer
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2]; // url + episode number id
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
